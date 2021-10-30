3b8a22c741eb9aeb33821d9823ad523c2e2a2c0f
Sergio Lamar <sergio.lamar@gmail.com>
2021-10-30 10:52:17 -0700

    js pseudo code

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//store variable references to all html elements
+
+//hide all elements not in use
+
+//user input and submit
+
+//save user input to local storage
+
+//load local storage and populate elements
+
+//get the value inside text box
+
+//send value to weather map api
+
+//receive respone from api
+
+//get lat and long data values from city name search
+
+//call opeanweather map one call api with lat & long
+
+//get response 
+
+//map respone data to correct html elements
+
+//show elements 
+
